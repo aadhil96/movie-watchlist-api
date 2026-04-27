1fe2c55c99b0b488a789818709180bf64f26dd55
aadhil96 <aadhil.imam@gmail.com>
2026-04-27 19:37:37 +0530

    validator added

--- movie_api/src/routes/watchlistRoute.js
+++ movie_api/src/routes/watchlistRoute.js
@@ -1,13 +1,14 @@
 import express from "express"
 import {addToWatchList ,removeFromWatchlist , updateWatchlistItem} from "../controller/watchlistController.js"
 import {autheMiddleware} from "../middleware/authMiddleware.js"
-
+import {validateRequest} from "../middleware/validateRequest.js"
+import {addToWatchlistSchema} from "../validators/watchlistValidators.js"
 
 const router = express.Router();
 
 router.use(autheMiddleware)
 
-router.post("/", addToWatchList )
+router.post("/", validateRequest(addToWatchList), addToWatchList )
 
 router.delete("/:id", removeFromWatchlist)
 
